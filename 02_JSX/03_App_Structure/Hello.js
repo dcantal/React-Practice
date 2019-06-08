0020131584b889c33d0e131fdeb5bee4ad8ca3b0
Dante Cantal <danteacantal@gmail.com>
2019-06-08 11:14:48 -0400

    app structure

---
+++ 02_JSX/03_App_Structure/Hello.js
@@ -0,0 +1,25 @@
+// Class Component
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello there!</h1>
+            </div>
+        );
+    }
+}
+
+// Functional Component
+
+// function Hello() {
+//     return (
+//         <div>
+//             <h1>Hello there!</h1>
+//             <h1>Hello there!</h1>
+//             <h1>Hello there!</h1>
+//         </div>
+//     )
+// }
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
