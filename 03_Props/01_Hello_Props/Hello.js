035a54de0e059c1d2e648aa7931167259f8c0c3d
Dante Cantal <danteacantal@gmail.com>
2019-06-08 18:27:19 -0400

    props practice

---
+++ 03_Props/01_Hello_Props/Hello.js
@@ -0,0 +1,11 @@
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi {this.props.to} from {this.props.from}</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
